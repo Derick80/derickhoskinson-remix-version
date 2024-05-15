df647c21ba8414cea4b4b6db3b2bffd9d2001c26
Derick <derickchoskinson@gmail.com>
2024-05-14 23:49:11 -0500

    update main

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import { json, type MetaFunction } from "@remix-run/node";
+import { useLoaderData } from '@remix-run/react';
 import { prisma } from '~/.server/prisma.server';
 
 export const meta: MetaFunction = () => {
@@ -23,7 +24,9 @@ export async function loader () {
 
 }
 
-export default function Index() {
+export default function Index () {
+  const { users } = useLoaderData<typeof loader>();
+  console.log(users,'users');
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
      <h1 className="text-3xl font-bold underline">
