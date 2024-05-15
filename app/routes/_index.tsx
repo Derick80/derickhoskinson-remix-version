a23aa964e4bde9a6e473efbb4eeadf42b475a3ee
Derick <derickchoskinson@gmail.com>
2024-05-14 22:43:22 -0500

    update tailwind

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,10 +10,10 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-     <h1 className="text-3xl font-bold underline">
+     <h1 className="text-3xl font-bold text-red-500 underline">
       Hello world!
       </h1>
-      <h2 className="text-2xl font-bold">
+      <h2 className="text-2xl font-bold text-purple-500">
         development environment
       </h2>
     </div>
