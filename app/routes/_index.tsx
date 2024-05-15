68cbe29aa72b602945f4963aad796ffdbdcaedc7
Derick <derickchoskinson@gmail.com>
2024-05-14 22:38:23 -0500

    update

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -12,7 +12,10 @@ export default function Index() {
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
      <h1 className="text-3xl font-bold underline">
       Hello world!
-    </h1>
+      </h1>
+      <h2 className="text-2xl font-bold">
+        development environment
+      </h2>
     </div>
   );
 }
